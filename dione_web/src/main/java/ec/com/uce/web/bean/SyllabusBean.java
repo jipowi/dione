2c226dc5ba1a3b909a435fde82c03a47fe4ab6b3
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-01-07 17:19:45 -0500

    Syllabus

--- dione_web/src/main/java/ec/com/uce/web/bean/SyllabusBean.java
+++ dione_web/src/main/java/ec/com/uce/web/bean/SyllabusBean.java
@@ -4,9 +4,18 @@
 package ec.com.uce.web.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+import org.primefaces.event.RowEditEvent;
+
+import ec.com.uce.ejb.dto.CompetenciaDTO;
+import ec.com.uce.ejb.dto.ObjetivoDTO;
 
 /**
  * <b> Permite manejar la informacion de la interfaz del cliente. </b>
@@ -26,6 +35,11 @@ public class SyllabusBean implements Serializable {
 	private String corequisito;
 	private String desAsigantura;
 	private String cedula;
+	private String objetivo;
+	private String competencia;
+
+	private static List<ObjetivoDTO> objetivosDTOs = new ArrayList<ObjetivoDTO>();
+	private static List<CompetenciaDTO> competenciasDTOs = new ArrayList<CompetenciaDTO>();
 
 	/**
 	 * @return the materia
@@ -117,4 +131,151 @@ public class SyllabusBean implements Serializable {
 		this.cedula = cedula;
 	}
 
+	/**
+	 * @return the objetivosDTOs
+	 */
+	public List<ObjetivoDTO> getObjetivosDTOs() {
+		return objetivosDTOs;
+	}
+
+	/**
+	 * @param objetivosDTOs
+	 *            the objetivosDTOs to set
+	 */
+	public static void setObjetivosDTOs(List<ObjetivoDTO> objetivosDTOs) {
+		SyllabusBean.objetivosDTOs = objetivosDTOs;
+	}
+
+	/**
+	 * @return the competenciasDTOs
+	 */
+	public List<CompetenciaDTO> getCompetenciasDTOs() {
+		return competenciasDTOs;
+	}
+
+	/**
+	 * @return the objetivo
+	 */
+	public String getObjetivo() {
+		return objetivo;
+	}
+
+	/**
+	 * @param objetivo
+	 *            the objetivo to set
+	 */
+	public void setObjetivo(String objetivo) {
+		this.objetivo = objetivo;
+	}
+
+	/**
+	 * @return the competencia
+	 */
+	public String getCompetencia() {
+		return competencia;
+	}
+
+	/**
+	 * @param competencia
+	 *            the competencia to set
+	 */
+	public void setCompetencia(String competencia) {
+		this.competencia = competencia;
+	}
+
+	/**
+	 * @param competenciasDTOs
+	 *            the competenciasDTOs to set
+	 */
+	public static void setCompetenciasDTOs(List<CompetenciaDTO> competenciasDTOs) {
+		SyllabusBean.competenciasDTOs = competenciasDTOs;
+	}
+
+	/**
+	 * 
+	 * <b> Permite agregar un registro de objetivos. </b>
+	 * <p>
+	 * [Author: Paul Jimenez, Date: 07/01/2015]
+	 * </p>
+	 * 
+	 */
+	public void addObjetivo() {
+		ObjetivoDTO item = new ObjetivoDTO(this.objetivo);
+		objetivosDTOs.add(item);
+
+		objetivo = "";
+	}
+
+	/**
+	 * 
+	 * <b> Permite editar un registro </b>
+	 * <p>
+	 * [Author: Paul Jimenez, Date: 07/01/2015]
+	 * </p>
+	 * 
+	 * @param event
+	 */
+	public void onEditObjetivo(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Item Edited", ((ObjetivoDTO) event.getObject()).getObjetivo());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	/**
+	 * 
+	 * <b> Permite eliminar un objetivo </b>
+	 * <p>
+	 * [Author: Paul Jimenez, Date: 07/01/2015]
+	 * </p>
+	 * 
+	 * @param event
+	 */
+	public void onCancelObjetivo(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Item Cancelled");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		objetivosDTOs.remove((ObjetivoDTO) event.getObject());
+	}
+	
+	/**
+	 * 
+	 * <b> Permite agregar un registro de competencias. </b>
+	 * <p>
+	 * [Author: Paul Jimenez, Date: 07/01/2015]
+	 * </p>
+	 * 
+	 */
+	public void addCompetencia() {
+		CompetenciaDTO item = new CompetenciaDTO(this.competencia);
+		competenciasDTOs.add(item);
+
+		competencia = "";
+	}
+
+	/**
+	 * 
+	 * <b> Permite editar un registro </b>
+	 * <p>
+	 * [Author: Paul Jimenez, Date: 07/01/2015]
+	 * </p>
+	 * 
+	 * @param event
+	 */
+	public void onEditCompetencia(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Item Edited", ((CompetenciaDTO) event.getObject()).getCompetencia());
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+	}
+
+	/**
+	 * 
+	 * <b> Permite eliminar un registro </b>
+	 * <p>
+	 * [Author: Paul Jimenez, Date: 07/01/2015]
+	 * </p>
+	 * 
+	 * @param event
+	 */
+	public void onCancelCompetencia(RowEditEvent event) {
+		FacesMessage msg = new FacesMessage("Item Cancelled");
+		FacesContext.getCurrentInstance().addMessage(null, msg);
+		competenciasDTOs.remove((CompetenciaDTO) event.getObject());
+	}
 }
