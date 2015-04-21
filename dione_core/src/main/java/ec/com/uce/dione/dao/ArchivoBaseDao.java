888ac93ac8a268fe6f662176078f3a6721f31177
paulJimenez666 <paul_jimenez777@hotmail.com>
2015-04-21 00:41:19 -0400

    Syllabus

---
+++ dione_core/src/main/java/ec/com/uce/dione/dao/ArchivoBaseDao.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.uce.dione.dao;
+
+import javax.ejb.Local;
+
+import ec.com.uce.dione.entities.ArchivoBase;
+
+/**
+ * <b> Interfaz local para operaciones con la tabla ArchivoBase. </b>
+ * 
+ * @author Paul Jimenez
+ * @version 1.0,Dec 10, 2014
+ * @since JDK1.6
+ */
+@Local
+public interface ArchivoBaseDao extends GenericDAO<ArchivoBase, Integer> {
+
+	
+}
---
+++ dione_core/src/main/java/ec/com/uce/dione/dao/impl/ArchivoBaseDaoImpl.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2014 JIPOVI Solutions - ECUADOR 
+ * Todos los derechos reservados
+ */
+package ec.com.uce.dione.dao.impl;
+
+import javax.ejb.Stateless;
+
+import ec.com.uce.dione.dao.ArchivoBaseDao;
+import ec.com.uce.dione.entities.ArchivoBase;
+
+/**
+ * <b> Incluir aqui la descripcion de la clase. </b>
+ * 
+ * @author Paul Jimenez
+ * @version 1.0,Dec 10, 2014
+ * @since JDK1.6
+ */
+@Stateless
+public class ArchivoBaseDaoImpl extends GenericDAOImpl<ArchivoBase, Integer> implements ArchivoBaseDao {
+
+}
